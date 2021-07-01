6d680b6db9fd45843230c7e01b4ec4d588a79356
jakeschultz89 <ejacobschultz@gmail.com>
2021-07-01 15:40:00 -0400

    full CRUD on movies

--- api/movies.js
+++ api/movies.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-
 // Models
 const { Movie } = require('../models');
 
@@ -36,19 +35,42 @@ const show = async(req, res) => {
     }
 }
 
-// const create = async(req, res) => {
-//     const { title, year, rated, genre } = req.body;
+const create = async(req, res) => {
+    const { Title, Year, Rated, Genre, Director, Writer, Actors, Plot, Language, Country, Awards, Poster, Metascore, imdbRating, imdbVotes, Type, DVD, BoxOffice, Production, Entertainment, Youtube, Num } = req.body;
 
-//     try {
-//         const newMovie = await Movie.create({ title, year, rated, genre });
-//         console.log('new favorite created', newMovie);
-//         res.json({ favorites: newMovie });
-//     } catch (error) {
-//         console.log('Error inside of POST of /api/movies');
-//         console.log(error);
-//         return res.status(400).json({ message: 'movie was not added. Please try again...' });
-//     }
-// }
+    try {
+        const newMovie = await Movie.create({ 
+            Title, 
+            Year, 
+            Rated, 
+            Genre, 
+            Director, 
+            Writer, 
+            Actors, 
+            Plot, 
+            Language, 
+            Country, 
+            Awards, 
+            Poster, 
+            Metascore, 
+            imdbRating, 
+            imdbVotes, 
+            Type, 
+            DVD, 
+            BoxOffice, 
+            Production, 
+            Entertainment, 
+            Youtube, 
+            Num 
+        });
+        console.log('new movie created', newMovie);
+        res.json({ movies: newMovie });
+    } catch (error) {
+        console.log('Error inside of POST of /api/movies');
+        console.log(error);
+        return res.status(400).json({ message: 'movie was not added. Please try again...' });
+    }
+}
 
 const search = async(req, res) => {
     const { Title } = req.body;
@@ -65,43 +87,41 @@ const search = async(req, res) => {
     }
 }
 
-// const update = async(req, res) => {
-//     console.log(req.body);
-//     try {
-//         const updatedMovie = await Movie.update({ title: req.body.title }, req.body); // updating the movie
-//         const movie = await Movie.findOne({ title: req.body.title });
+const update = async(req, res) => {
+    console.log(req.body);
+    try {
+        const updatedMovie = await Movie.findByIdAndUpdate( req.params.id, req.body); // updating the movie
+        const movie = await Movie.findById(req.params.id);
 
-//         console.log(updatedMovie); // { n: 1, nModified: 0, ok: 1 }
-//         console.log(movie); // a movie object 
+        console.log(updatedMovie); // { n: 1, nModified: 0, ok: 1 }
+        console.log(movie); // a movie object 
 
-//         res.redirect(`/api/movies/${movie.id}`);
+        res.redirect(`/api/movies/${movie.id}`);
 
-//     } catch (error) {
-//         console.log('Error inside of UPDATE route');
-//         console.log(error);
-//         return res.status(400).json({ message: 'movie could not be updated. Please try again...' });
-//     }
-// }
+    } catch (error) {
+        console.log('Error inside of UPDATE route');
+        console.log(error);
+        return res.status(400).json({ message: 'movie could not be updated. Please try again...' });
+    }
+}
 
-// const deleteMovie = async(req, res) => {
-//     const { id } = req.params;
-//     try {
-//         console.log(id);
-//         const result = await Movie.findByIdAndRemove(id);
-//         console.log(result);
-//         res.redirect('/api/movies');
-//     } catch (error) {
-//         console.log('inside of DELETE route');
-//         console.log(error);
-//         return res.status(400).json({ message: 'movie was not deleted. Please try again...' });
-//     }
-// }
+const deleteMovie = async(req, res) => {
+    const { id } = req.params;
+    try {
+        console.log(id);
+        const result = await Movie.findByIdAndRemove(id);
+        console.log(result);
+        res.redirect('/api/movies');
+    } catch (error) {
+        console.log('inside of DELETE route');
+        console.log(error);
+        return res.status(400).json({ message: 'movie was not deleted. Please try again...' });
+    }
+}
 
 // GET api/movies/test (Public)
-router.get('/test', (req, res) => {
-    res.json({ msg: 'movie endpoint OK!' });
+router.get('/test', (req, res) => {res.json({ msg: 'movie endpoint OK!' });
 });
-
 
 // GET -> /api/movies/
 router.get('/', passport.authenticate('jwt', { session: false }), index);
@@ -109,10 +129,10 @@ router.get('/', passport.authenticate('jwt', { session: false }), index);
 router.get('/:id', passport.authenticate('jwt', { session: false }), show);
 // POST -> /api/search
 router.post('/search', passport.authenticate('jwt', { session: false }), search)
-    // POST -> /api/movies
+// POST -> /api/movies
 router.post('/', passport.authenticate('jwt', { session: false }), create);
 // PUT -> /api/movies
-router.put('/', passport.authenticate('jwt', { session: false }), update);
+router.put('/:id', passport.authenticate('jwt', { session: false }), update);
 // DELETE => /api/movies/:id
 router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteMovie);
 
